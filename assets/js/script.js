aa028c7ac99af6d6cd35a02906296ac5f7daa26a
Junghoon Yoon <jyoon3@2u.com>
2022-06-29 10:03:37 -0400

    Form information now saved to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,27 @@ function submitPikachuInfo(event) {
 
     console.log(inputPikachuDetails);
     console.log(inputPikachuType);
+
+    var pikachuObject = {};
+    pikachuObject["pikachuType"] = inputPikachuType;
+
+    pikachuObject["pikachuDetails"] = inputPikachuDetails;
+
+    console.log(pikachuObject);
+    // Setting localstorage directly in the same function seems ugly
+    // Let's make an external function!
+    // localStorage.setItem("pikachuObject", JSON.stringify(pikachuObject));
+    setLocalStorage(pikachuObject);
 }
 
+function setLocalStorage(receivePikachuObject) {
+    console.log("This is in another function", receivePikachuObject);
+    localStorage.setItem("pikachuObject", JSON.stringify(receivePikachuObject));
+}
 
-
-
-
-
+function getInfoFromLocalStorage() {
+    localStorage.getItem('')
+}
 
 
 // When using a form, you need to target the PARENT form itself, NOT the form button. 
